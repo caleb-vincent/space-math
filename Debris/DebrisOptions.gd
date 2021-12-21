41bac171867fd42701eba1600ab57bdab4390920
Caleb Vincent <caleb.i.m.vincent@gmail.com>
2021-12-21 14:59:05 -0500

    Save options between game runs

--- Debris/DebrisOptions.gd
+++ Debris/DebrisOptions.gd
@@ -1,10 +1,18 @@
 extends Control
 
+const settings_file = "user://DebrisOptions.save"
+
 var maxAnswer : int = 10
 var minAnswer : int = 0
 
 
+################################################################################
+#	Inherited Methods
+################################################################################
+
+
 func _ready() -> void:
+	_load_settings()
 	$CenterContainer/VBoxContainer/MaxAnswerSelection.value = maxAnswer
 	$CenterContainer/VBoxContainer/MinAnswerSelection.value = minAnswer
 
@@ -13,8 +21,34 @@ func _process(delta):
 	if Input.is_action_just_released("ui_cancel"):
 		_exit()
 
+################################################################################
+#	Private Methods
+################################################################################
+
+
+func _save_settings():
+	var f = File.new()
+	f.open(settings_file, File.WRITE)
+	f.store_var(minAnswer)
+	f.store_var(maxAnswer)
+	f.close()
+	
+	
+func _load_settings():
+	var f = File.new()
+	if f.file_exists(settings_file):
+		f.open(settings_file, File.READ)
+		minAnswer = f.get_var()
+		maxAnswer = f.get_var()
+
+
+################################################################################
+#	Signal Handling
+################################################################################
+
 
 func _exit() -> void:
+	_save_settings()
 	visible = false
 
 
--- Dodge/DodgeOptions.gd
+++ Dodge/DodgeOptions.gd
@@ -11,14 +11,53 @@ enum GameType {
 var selectedGameType = GameType.ADDITION
 var maxAnswer : int = 10
 
+const settings_file = "user://DodgeOptions.save"
+
+
+################################################################################
+#	Inherited Methods
+################################################################################
+
 
 func _ready() -> void:
+	_load_settings()
 	$CenterContainer/VBoxContainer/MaxAnswerSelection.value = maxAnswer
+	$CenterContainer/VBoxContainer/GameTypeSelection.select(selectedGameType)
 
 
 func _process(delta):
 	if Input.is_action_just_released("ui_cancel"):
 		_exit()
+
+
+################################################################################
+#	Public Methods
+################################################################################
+
+
+################################################################################
+#	Private Methods
+################################################################################
+
+func _save_settings():
+	var f = File.new()
+	f.open(settings_file, File.WRITE)
+	f.store_var(selectedGameType)
+	f.store_var(maxAnswer)
+	f.close()
+	
+	
+func _load_settings():
+	var f = File.new()
+	if f.file_exists(settings_file):
+		f.open(settings_file, File.READ)
+		selectedGameType = f.get_var()
+		maxAnswer = f.get_var()
+
+
+################################################################################
+#	Signal Handling
+################################################################################
 
 
 func _on_GameType_selected(id: int) -> void:
@@ -36,4 +75,5 @@ func  _on_MaxAnswer_changed(id: float) -> void:
 
 
 func _exit() -> void:
+	_save_settings()
 	visible = false
